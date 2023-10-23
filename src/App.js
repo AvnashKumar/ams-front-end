3af040573ad457a4d033696a237ebcfd5f40f31e
Avnash Kumar <avnashk.ee.21@nitj.ac.in>
2023-10-23 20:15:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import PostAttendance from "./facultySidePages/PostAttendance";
+import LoadingIcon from "./utils/Loadingicon";
+
+function App() {
+  return (
+    <div >
+     <PostAttendance/>
+       </div>
+  );
+}
+
+export default App;
---
+++ src/facultySidePages/PostAttendance.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FieldSelection from './components/FieldSelection'
+
+function PostAttendance() {
+  return (
+    <div>
+        <FieldSelection/>
+    </div>
+  )
+}
+
+export default PostAttendance
